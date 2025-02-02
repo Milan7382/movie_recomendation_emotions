e3b726f1229b3e4a16325e93725bc637d953bc75
Milan7382 <a.milandora92@gmail.com>
2025-02-02 17:34:07 +0530

    changes

--- app/movie_main.py
+++ app/movie_main.py
@@ -1,3 +1,7 @@
+import sys
+import os
+sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '..', 'model')))
+
 import streamlit as st
 from ..model.try_new import recommend, get_clean_data  # Import get_clean_data here
 import pickle
